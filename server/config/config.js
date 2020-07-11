1473d0e109032df2b186fa928d31302c1b023d8b
RAVC <rravc00@hotmail.com>
2020-07-10 22:09:46 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,20 @@
 
 
-
+//========= Port =======
 process.env.PORT = process.env.PORT || 3000;
+
+
+//========= Entorno =======
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ======== Base de Datos =====
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev' ){
+  urlDB ='mongodb://localhost:27017/cafe'
+} else {
+  urlDB = 'mongodb+srv://cafe-user:123456asdf@cluster0.yb19t.mongodb.net/cafe?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
+const colors = require('colors/safe');
+
 const app = express()
+
 var bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
@@ -10,42 +14,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
+app.use (require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-})
 
-app.post('/usuario', function (req, res) {
-
-  let body = req.body
-
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok:false,
-      mensaje:'el nombre es necesario'
-    })
-  }else{
-    res.json({
-      persona: body
-    });
-  }
-    
-})
-
-app.put('/usuario/:id', function (req, res) {
-  
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-})
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuario');
-})
- 
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
+    console.log(colors.cyan.bold("===================="));
+    console.log(colors.underline.cyan.bold('Base de datos ONLINE'));
+});
 
 app.listen(process.env.PORT, () => {
-  console.log("escuchando port: ", process.env.PORT);
+  console.log(colors.underline.cyan.bold("______________________"));
+  console.log(colors.underline.cyan.bold("escuchando port: ", process.env.PORT));
 })
